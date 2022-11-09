c75a82766c91a886c3f23413ec711c5270bed577
jeremy <boo--boo@hotmail.fr>
2022-11-09 19:38:57 +0100

    done with the meta classes

--- robust_singleton_decorator/MetaClasses.py
+++ robust_singleton_decorator/MetaClasses.py
@@ -74,3 +74,51 @@ class SingletonWithNonSingletonChildren(type):
             classdict["__new__"] = make_ignore_extra_args_wrapper(old_new)
 
         return type.__new__(cls, name, bases, classdict)
+
+
+# * Define the new function that will be used to overwrite the __new__ function
+# * in the classes created by the metaclasses below
+def new(class_, *args, **kwargs):
+    if not hasattr(class_, "instance") or class_.instance is None:
+        old_new = make_ignore_extra_args_wrapper(getattr(class_, "__old_new"))
+        class_.instance = old_new(class_, *args, **kwargs)
+    return class_.instance
+
+
+class SingletonFinal(type):
+    def __new__(cls, name, bases, classdict):
+        for b in bases:
+            if isinstance(b, SingletonFinal):
+                raise TypeError("type '{0}' is not an acceptable base type".format(b.__name__))
+
+        old_class = type.__new__(cls, name, bases, classdict)
+        classdict["__new__"] = new
+        classdict["__old_new"] = old_class.__new__
+
+        return type.__new__(cls, name, bases, dict(classdict))
+
+
+class SingletonWithSingletonChildren(type):
+    def __new__(cls, name, bases, classdict):
+        old_class = type.__new__(cls, name, bases, classdict)
+
+        if not bases or not any([isinstance(b, SingletonWithSingletonChildren) for b in bases]):
+            classdict["__new__"] = new
+            classdict["__old_new"] = old_class.__new__
+        else:
+            classdict["instance"] = None
+
+        return type.__new__(cls, name, bases, classdict)
+
+
+class SingletonWithNonSingletonChildren(type):
+    def __new__(cls, name, bases, classdict):
+        old_class = type.__new__(cls, name, bases, classdict)
+
+        if not bases or not any([isinstance(b, SingletonWithNonSingletonChildren) for b in bases]):
+            classdict["__new__"] = new
+            classdict["__old_new"] = old_class.__new__
+        else:
+            classdict["__new__"] = make_ignore_extra_args_wrapper(getattr(old_class, "__old_new"))
+
+        return type.__new__(cls, name, bases, classdict)
