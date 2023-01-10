43c5f530e91116258d4420dd55119d18cee65b73
jeremy <boo--boo@hotmail.fr>
2023-01-10 23:42:01 +0100

    finised second refactoring

--- robust_singleton_decorator/singleton.py
+++ robust_singleton_decorator/singleton.py
@@ -1,29 +1,18 @@
-import logging
-from robust_singleton_decorator.MetaClasses import (
-    SingletonFinal,
-    SingletonWithNonSingletonChildren,
-    SingletonWithSingletonChildren,
-)
-
-MUST_CHILDREN_BE_SINGLETON_DEFAULT = False
+from robust_singleton_decorator.MetaClasses import MakeSingleton, MakeFinalSingleton
 
 
-def singleton(class_=None, /, *, is_final=True, must_children_be_singleton=MUST_CHILDREN_BE_SINGLETON_DEFAULT):
+def singleton(class_=None, /, *, is_final=True):
     """Transform an input class into a singleton class. Can be used as a decorator
 
     Args:
         class_ (Class): A class as a positional argument, whose behavior will be modified
         is_final (bool): Keyword argument. If True the class cannot be used as a base class for inheritance.
             WARNING: Setting it to False can lead to unexpected behaviour for the Child class. Default to True.
-        must_children_be_singleton (bool, optional): Keyword argument. Whether the children class of the current
-            singleton class must also be singleton.
+
 
     Returns:
         NewClass: a subclass of the input that acts as a singleton.
     """
-
-    if is_final and must_children_be_singleton is not MUST_CHILDREN_BE_SINGLETON_DEFAULT:
-        logging.warning("`is_final` is set to True, so the `must_children_be_singleton` argument is ignored")
 
     def wrapper(class_):
         """Take a class as input. Creates and returns a subclass
@@ -35,33 +24,9 @@ def singleton(class_=None, /, *, is_final=True, must_children_be_singleton=MUST_
         Return
             NewClass: a subclass of the input that acts as a singleton.
         """
-        # * Check that class_ is not already created fom on the of the Singleton metaclass
-        _check_metaclass(class_)
 
         # * Dynamically create the new class using on of the meta classes
-        MetaClass = _get_metaclass(is_final, must_children_be_singleton)
-        return MetaClass(class_.__name__, (class_,), dict())
+        MetaClass = MakeFinalSingleton if is_final else MakeSingleton
+        return MetaClass(class_.__name__, (class_,), dict(), make_singleton=True)
 
     return wrapper(class_) if class_ is not None else wrapper
-
-
-def _get_metaclass(is_final, must_children_be_singleton):
-    if is_final:
-        MetaClass = SingletonFinal
-    elif not is_final and must_children_be_singleton:
-        MetaClass = SingletonWithSingletonChildren
-    else:
-        MetaClass = SingletonWithNonSingletonChildren
-
-    return MetaClass
-
-
-def _check_metaclass(class_) -> None:
-    for metaclass in [SingletonFinal, SingletonWithSingletonChildren, SingletonWithNonSingletonChildren]:
-        if isinstance(class_, metaclass):
-            raise TypeError(
-                f"The class {class_.__name__} is an instance of {metaclass.__name__}. "
-                f"You cannot use the singleton decorator on classes that derive from instances of {metaclass}. "
-                "Please use the singleton decorator on the base class, and use the arguments `is_final` "
-                "and ` must_children_be_singleton` to define the behavior of its children classes."
-            )
--- tests/test_meta_classes.py
+++ tests/test_meta_classes.py
@@ -1,144 +1,6 @@
 import pytest
 
-from robust_singleton_decorator.MetaClasses import (
-    SingletonWithSingletonChildren,
-    SingletonWithNonSingletonChildren,
-    SingletonFinal,
-    MakeSingleton,
-    MakeFinalSingleton,
-)
-
-
-def test_singleton_final_basic():
-    class A(metaclass=SingletonFinal):
-        def __init__(self, x, y):
-            self.x = x
-            self.y = y
-
-    a = A(1, 2)
-    a2 = A(1, 2)
-    assert (a.x, a.y) == (1, 2)
-    assert a2 is a
-
-    # Should raise an error when trying to make a children class
-    with pytest.raises(TypeError):
-
-        class B(A):
-            ...
-
-
-def test_singleton_children_basic():
-    class A(metaclass=SingletonWithSingletonChildren):
-        def __init__(self, x, y):
-            self.x = x
-            self.y = y
-
-    a = A(1, 2)
-    a2 = A(1, 2)
-    assert (a.x, a.y) == (1, 2)
-    assert a2 is a
-
-    class B(A):
-        ...
-
-    b = B(2, 3)
-    b2 = B(2, 3)
-    assert b.x, b.y == (2, 3)
-    assert b is b2
-
-    assert a is not b
-
-
-def test_singleton_with_singleton_children():
-
-    # Try to get a weird definition where one attribut is defined in __new__ and the others in __init__
-    class A(metaclass=SingletonWithSingletonChildren):
-        def __new__(cls, x, y, z):
-            obj = super().__new__(cls)
-            obj.z = z
-            return obj
-
-        def __init__(self, x, y, z):
-            self.x = x
-            self.y = y
-
-    a = A(1, 2, 3)
-    assert (a.x, a.y, a.z) == (1, 2, 3)
-
-    # check that A instances are the same
-    a2 = A(1, 2, 3)
-    assert a2 is a
-
-    class B(A):
-        def __new__(cls, x, y, z):
-            # return the super().__new__ with x, y, z = 0, 0, 0 as input
-            # Use the __init__ to overwrite the x, y, z attributes of the object
-            return super(B, cls).__new__(cls, 0, 0, 0)
-
-        def __init__(self, x, y, z):
-            self.x = x
-            self.y = y
-            self.z = z
-
-    b = B(2, 3, 4)
-    assert (b.x, b.y, b.z) == (2, 3, 4)
-
-    # check that B is still a singleton class
-    b2 = B(2, 3, 4)
-    assert b2 is b
-
-    class C(B):
-        ...
-
-    # check that C is still a singleton, but different from A
-    c = C(1, 2, 3)
-    c2 = C(1, 2, 3)
-    assert a is not c
-    assert c is c2
-
-
-def test_singleton_with_non_singleton_children_basic():
-    class A(metaclass=SingletonWithNonSingletonChildren):
-        def __init__(self, x, y):
-            self.x = x
-            self.y = y
-
-    # Check that A is a singleton
-    a = A(1, 2)
-    a2 = A(1, 2)
-    assert (a.x, a.y) == (1, 2)
-    assert a2 is a
-
-    class B(A):
-        ...
-
-    # check that b is not a
-    b = B(1, 2)
-    assert b.x, b.y == (1, 2)
-    assert a is not b
-
-    # check that a second instance of B is not b
-    b2 = B(2, 3)
-    assert b2.x, b2.y == (2, 3)
-    assert b2 is not b
-
-
-def test_singleton_with_non_singleton_children():
-    class A(metaclass=SingletonWithNonSingletonChildren):
-        def __init__(self, x, y):
-            self.x = x
-            self.y = y
-
-    class B(A):
-        ...
-
-    class C(B):
-        ...
-
-    # check that C is not a singleton class
-    c = C(1, 2)
-    c2 = C(1, 2)
-    assert c is not c2
+from robust_singleton_decorator.MetaClasses import MakeSingleton, MakeFinalSingleton
 
 
 def test_make_singleton_class():
@@ -168,13 +30,14 @@ def test_make_singleton_class():
     c2 = C(3, 4)
     assert c is c2
 
-    # test that we can make a SingletonFinal inheriting from a Singleton
+    # test that we can make a SingletonFinal inheriting from a Singleton class
 
     class D(C, metaclass=MakeFinalSingleton):
         ...
 
-    d = D(1, 2)
     assert isinstance(D, MakeFinalSingleton)
+
+    d = D(1, 2)
     d2 = D(3, 4)
     assert d is d2  # check that D is a singleton
 
@@ -202,7 +65,7 @@ def test_make_singleton_final():
     b2 = B(3, 4)
     assert b is b2  # check that D is a singleton
 
-    # check we cannot inherit from B
+    # Should raise an error when trying to make a children class
     with pytest.raises(TypeError):
 
         class C(B):
