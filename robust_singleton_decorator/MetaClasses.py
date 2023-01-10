8fc22fb0d118e672c97db9140625f16d93d4259e
jeremy <boo--boo@hotmail.fr>
2023-01-10 23:07:45 +0100

    added new metaclasses for this small refactoring

--- robust_singleton_decorator/MetaClasses.py
+++ robust_singleton_decorator/MetaClasses.py
@@ -74,3 +74,39 @@ class SingletonWithNonSingletonChildren(type):
             classdict["__new__"] = make_ignore_extra_args_wrapper(old_new)
 
         return type.__new__(cls, name, bases, classdict)
+
+
+class MakeSingleton(type):
+    """Metaclass creating singleton classes whose children are **not** singleton classes."""
+
+    def __new__(cls, name, bases, classdict, make_singleton: bool = False):
+        old_class = type.__new__(cls, name, bases, classdict)
+
+        # Make the singleton class if make_singleton
+        if make_singleton:
+            classdict["_old_new"] = old_class.__new__ if "__new__" not in classdict else classdict["__new__"]
+            classdict["__new__"] = new
+            return type.__new__(cls, name, bases, classdict)
+
+        # if not make_singleton, simply forward the __new__/_old_new class of the old_class to the new one.
+        if "__new__" in classdict:
+            log.warning(
+                "You are overwriting the __new__ method of a class that inherits from a singleton class."
+                " This may lead to unexpected behavior"
+            )
+            old_new = classdict["__new__"]
+        else:
+            old_new = getattr(old_class, "_old_new", old_class.__new__)
+
+        classdict["__new__"] = make_ignore_extra_args_wrapper(old_new)
+
+        return type.__new__(cls, name, bases, classdict)
+
+
+class MakeFinalSingleton(MakeSingleton):
+    def __new__(cls, name, bases, classdict, make_singleton: bool = True):
+        for b in bases:
+            if isinstance(b, MakeFinalSingleton):
+                raise TypeError("type '{0}' is not an acceptable base type".format(b.__name__))
+
+        return super().__new__(cls, name, bases, classdict, make_singleton)
--- tests/test_meta_classes.py
+++ tests/test_meta_classes.py
@@ -4,6 +4,8 @@ from robust_singleton_decorator.MetaClasses import (
     SingletonWithSingletonChildren,
     SingletonWithNonSingletonChildren,
     SingletonFinal,
+    MakeSingleton,
+    MakeFinalSingleton,
 )
 
 
@@ -137,3 +139,71 @@ def test_singleton_with_non_singleton_children():
     c = C(1, 2)
     c2 = C(1, 2)
     assert c is not c2
+
+
+def test_make_singleton_class():
+    def init_redef(self, x, y):
+        self.x = x
+        self.y = y
+
+    A = MakeSingleton("A", (), {"__init__": init_redef}, make_singleton=True)
+
+    # check that A is a singleton. Should be the case since we've set make_singleton=True above
+    a = A(1, 2)
+    a2 = A(2, 3)
+    assert a is a2
+
+    # check that a class inheriting from A is not a singleton
+    class B(A):
+        ...
+
+    b = B(1, 2)
+    b2 = B(2, 3)
+    assert b is not b2
+
+    # check that we can make a singleton inheriting from B
+    C = MakeSingleton("C", (B,), {}, make_singleton=True)
+
+    c = C(2, 3)
+    c2 = C(3, 4)
+    assert c is c2
+
+    # test that we can make a SingletonFinal inheriting from a Singleton
+
+    class D(C, metaclass=MakeFinalSingleton):
+        ...
+
+    d = D(1, 2)
+    assert isinstance(D, MakeFinalSingleton)
+    d2 = D(3, 4)
+    assert d is d2  # check that D is a singleton
+
+
+def test_make_singleton_final():
+    def init_redef(self, x, y):
+        self.x = x
+        self.y = y
+
+    # check that we can make a singleton inheriting from B
+    A = MakeSingleton("A", (), {"__init__": init_redef}, make_singleton=True)
+
+    a = A(2, 3)
+    a2 = A(3, 4)
+    assert a is a2
+
+    # test that we can make a SingletonFinal inheriting from a Singleton
+
+    class B(A, metaclass=MakeFinalSingleton):
+        ...
+
+    assert isinstance(B, MakeFinalSingleton)
+
+    b = B(1, 2)
+    b2 = B(3, 4)
+    assert b is b2  # check that D is a singleton
+
+    # check we cannot inherit from B
+    with pytest.raises(TypeError):
+
+        class C(B):
+            ...
