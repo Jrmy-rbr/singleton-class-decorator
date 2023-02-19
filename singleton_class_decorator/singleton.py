b35ce9441b442fd9d150eea0726c525b9e0c5e3f
jeremy <boo--boo@hotmail.fr>
2023-02-19 22:10:29 +0100

    Polishing code, and adding 100 percent coverage requirement

--- singleton_class_decorator/singleton.py
+++ singleton_class_decorator/singleton.py
@@ -2,31 +2,31 @@ from typing import Type, Optional
 from singleton_class_decorator.MetaClasses import _get_metaclasses
 
 
-def singleton(class_: Optional[Type] = None, /, *, is_final: bool = True):
+def singleton(klass: Optional[Type] = None, /, *, is_final: bool = True):
     """Transform an input class into a singleton class. Can be used as a decorator
 
     Args:
-        class_ (Optional[Type]): A class as a positional argument, whose behavior will be modified
+        klass (Optional[Type]): A class as a positional argument, whose behavior will be modified
         is_final (bool): Keyword argument. If True the class cannot be used as a base class for inheritance.
 
     Returns:
         NewClass: a subclass of the input that acts as a singleton.
     """
 
-    def wrapper(class_: Type):
+    def wrapper(klass: Type):
         """Take a class as input. Creates and returns a subclass
         that acts as a singleton.
 
         Args:
-            class_ (Type): The class that we want to modify
+            klass (Type): The class that we want to modify
 
         Return
             Type: a subclass of the input that acts as a singleton.
         """
-        # Dynamically create the Metaclass, so that MetaClass inherit from type(class_)
+        # Dynamically create the Metaclass, so that MetaClass inherit from type(klass)
         # This is done so that singleton can be used on classes that use another meta-class
-        MetaClass = _get_metaclasses(class_)[int(is_final)]
+        MetaClass = _get_metaclasses(klass)[int(is_final)]
         # Dynamically create the new class using on of the MetaClass
-        return MetaClass(class_.__name__, (class_,), dict(), make_singleton=True)
+        return MetaClass(klass.__name__, (klass,), dict(), make_singleton=True)
 
-    return wrapper(class_) if class_ is not None else wrapper
+    return wrapper(klass) if klass is not None else wrapper
