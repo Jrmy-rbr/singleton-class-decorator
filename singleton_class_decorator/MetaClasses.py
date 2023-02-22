556d2d0d1e2cf8d3ef7ef9b63223bb0eed3b1034
Jeremy <63064766+Jrmy-rbr@users.noreply.github.com>
2023-02-22 08:14:48 +0100

    Continue Code polishing (#19)

--- singleton_class_decorator/MetaClasses.py
+++ singleton_class_decorator/MetaClasses.py
@@ -3,7 +3,7 @@ from typing import Type
 from hashlib import sha256
 
 INSTANCE_NAME = "instance" + sha256("instance".encode()).hexdigest()  # the hash is used for the unicity of the name
-
+OLD_NEW_NAME = "old_new" + sha256("old_new".encode()).hexdigest()  # the hash is used for the unicity of the name
 """
 Two meta-classes that allow to define the classes output by the singleton decorator.
 """
@@ -13,21 +13,21 @@ Two meta-classes that allow to define the classes output by the singleton decora
 # * in the classes created by the metaclasses returned by _get_metaclasses
 
 
-def new(class_: Type, *args, **kwargs):
-    # act as class_._old_new in the
-    # case in which the class_ redefines its __new__ method,
+def new(klass: Type, *args, **kwargs):
+    # act as getattr(klass, OLD_NEW_NAME) in the
+    # case in which the klass redefines its __new__ method,
     # and for which the __new__ method of its *parent* class was *this* new function
     # This is because a singleton's child should not be a singleton itself
-    if "__new__" in class_.__dict__ and class_.__new__ is not new and class_.__mro__[1].__new__ is new:
-        old_new = make_ignore_extra_args_wrapper(class_._old_new)
-        return old_new(class_, *args, **kwargs)
+    if "__new__" in klass.__dict__ and klass.__new__ is not new and klass.__mro__[1].__new__ is new:
+        old_new = make_ignore_extra_args_wrapper(getattr(klass, OLD_NEW_NAME))
+        return old_new(klass, *args, **kwargs)
 
     # Cash the first instance of the class
-    if not hasattr(class_, INSTANCE_NAME):
-        old_new = make_ignore_extra_args_wrapper(class_._old_new)
-        setattr(class_, INSTANCE_NAME, old_new(class_, *args, **kwargs))
+    if not hasattr(klass, INSTANCE_NAME):
+        old_new = make_ignore_extra_args_wrapper(getattr(klass, OLD_NEW_NAME))
+        setattr(klass, INSTANCE_NAME, old_new(klass, *args, **kwargs))
 
-    return getattr(class_, INSTANCE_NAME)
+    return getattr(klass, INSTANCE_NAME)
 
 
 # * Define the __new__ functions that'll be used in the Metaclasses defined below
@@ -38,21 +38,21 @@ def make_singleton__new__(meta_cls, name, bases, classdict, make_singleton: bool
 
     # Make the singleton class if make_singleton
     if make_singleton:
-        classdict["_old_new"] = old_class.__new__ if "__new__" not in classdict else classdict["__new__"]
+        classdict[OLD_NEW_NAME] = old_class.__new__ if "__new__" not in classdict else classdict["__new__"]
         classdict["__new__"] = new
         return type(meta_cls).__new__(meta_cls, name, bases, classdict)
 
-    # if not make_singleton, simply forward the __new__/_old_new class of the old_class to the new one.
-    old_new = classdict["__new__"] if "__new__" in classdict else getattr(old_class, "_old_new", old_class.__new__)
+    # if not make_singleton, simply forward the __new__/old_new class of the old_class to the new one.
+    old_new = classdict["__new__"] if "__new__" in classdict else getattr(old_class, OLD_NEW_NAME, old_class.__new__)
     classdict["__new__"] = make_ignore_extra_args_wrapper(old_new)
 
     return type(meta_cls).__new__(meta_cls, name, bases, classdict)
 
 
 def make_final_singleton__new__(meta_cls, name, bases, classdict, make_singleton: bool = True):
-    for b in bases:
-        if isinstance(b, meta_cls):
-            raise TypeError("type '{0}' is not an acceptable base type".format(b.__name__))
+    for base in bases:
+        if isinstance(base, meta_cls):
+            raise TypeError(f"type '{base.__name__}' is not an acceptable base type")
 
     return super(meta_cls, meta_cls).__new__(meta_cls, name, bases, classdict, make_singleton)
 
